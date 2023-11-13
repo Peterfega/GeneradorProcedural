891953c952dadde1308539d98bd09b71be9418be
Peterfega <peterfega@hotmail.com>
2023-11-13 16:10:29 +0100

    First Perlin code working

--- GeneradorProcedural/test.cpp
+++ GeneradorProcedural/test.cpp
@@ -44,6 +44,57 @@ bool InitOpenGL()
 
 int main()
 {
+    // Definir las dimensiones de la malla
+    const int gridSize = 128;
+
+    // Recopilar datos de ruido Perlin
+    std::vector<float> noiseData(128 * 128 * 128);
+    int index = 0;
+
+    // Create and configure FastNoise object
+    FastNoiseLite noise;
+    noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+
+
+    for (int y = 0; y < 128; y++)
+    {
+        for (int x = 0; x < 128; x++)
+        {
+            for (int z = 0; z < 128; z++)
+            {
+                noiseData[index++] = noise.GetNoise((float)x, (float)y, (float)z);
+            }
+        }
+    }
+
+    // Crear una malla 3D con vértices
+    std::vector<GLfloat> vertices;
+    for (int z = 0; z < gridSize; z++) {
+        for (int y = 0; y < gridSize; y++) {
+            for (int x = 0; x < gridSize; x++) {
+                float xf = static_cast<float>(x) / static_cast<float>(gridSize);
+                float yf = static_cast<float>(y) / static_cast<float>(gridSize);
+                float zf = static_cast<float>(z) / static_cast<float>(gridSize);
+
+                // Obtener el valor de ruido Perlin correspondiente
+                float noiseValue = noiseData[x + y * gridSize + z * gridSize * gridSize];
+
+                // Ajustar el valor de ruido según tus necesidades
+                float scaleFactor = 10.0f;
+                float height = noiseValue * scaleFactor;
+
+                // Agregar el vértice a la lista de vértices
+                vertices.push_back(x);
+                vertices.push_back(height);
+                vertices.push_back(z);
+            }
+        }
+    }
+
+
+
+    ///GLWF STUFF
+
     if (!InitOpenGL())
     {
         return -1;
@@ -52,21 +103,6 @@ int main()
     // Bucle principal de renderización
     while (!glfwWindowShouldClose(glfwGetCurrentContext()))
     {
-        // Create and configure FastNoise object
-        FastNoiseLite noise;
-        noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
-
-        // Gather noise data
-        std::vector<float> noiseData(128 * 128);
-        int index = 0;
-
-        for (int y = 0; y < 128; y++)
-        {
-            for (int x = 0; x < 128; x++)
-            {
-                noiseData[index++] = noise.GetNoise((float)x, (float)y);
-            }
-        }
 
         // Intercambia los buffers
         glfwSwapBuffers(glfwGetCurrentContext());
